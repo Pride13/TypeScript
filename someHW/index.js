eb8011b1f40f7488aec71e6c88d6c2c2a8997e46
Vasyl <vasilkuzyk@gmail.com>
2019-10-24 15:22:17 +0300

    first on ts

--- someHW/index.js
+++ someHW/index.js
@@ -1,1 +1,22 @@
-var q = 1;
+//string
+// let str = 'string!';
+// // str = 10; will be an issue
+// str = 'string!';
+var str = 'string!'; // write a type obviously
+//number
+var num = 10;
+//boolean
+var isTrue = true;
+var isFalse = false;
+isTrue = false; // can be
+// isTrue = 1; can't be
+//other
+//first way
+// let unknown: any = '1';
+// unknown = 1;
+// unknown = true;
+//second way
+var unknown = '1';
+unknown = 1;
+unknown = 'hello';
+unknown = true;
--- someHW/index.ts
+++ someHW/index.ts
@@ -1,1 +1,29 @@
-const q = 1;
+//string
+// let str = 'string!';
+// // str = 10; will be an issue
+// str = 'string!';
+let str:string = 'string!'; // write a type obviously
+
+//number
+let num: number = 10;
+
+//boolean
+let isTrue: boolean = true;
+let isFalse: boolean = false;
+isTrue = false; // can be
+// isTrue = 1; can't be
+
+//other
+//first way
+// let unknown: any = '1';
+// unknown = 1;
+// unknown = true;
+
+//second way
+let unknown: string | number | boolean = '1';
+unknown = 1;
+unknown = 'hello';
+unknown = true;
+
+
+
